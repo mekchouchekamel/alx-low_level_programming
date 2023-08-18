7c41d8dc2a015a4035544e358688d13fb73a0a4b
mekchouchekamel <kk9515825@gmail.com>
2023-08-17 17:20:57 -0700

    isupper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,7 +1,12 @@
 #include "main.h"
-int _isupper (int c)
-{if (c >='A' && c<='Z')
-	{ return (1);
+int _isupper(int c)
+{
+if (c >= 'A' && c <= 'Z')
+{
+return (1);
 }
-else { return (0);
-}}
+else
+{
+return (0);
+}
+}
