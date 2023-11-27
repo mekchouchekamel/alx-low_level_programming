7b47538948b1cd7911d0f0f5eb91a5e655dcbd17
mekchouchekamel <kk9515825@gmail.com>
2023-11-27 12:29:11 -0800

    0x15-file_io

--- 0-read_textfile.c
+++ 0-read_textfile.c
@@ -2,7 +2,8 @@
 
 /**
  * read_textfile - reads a text file and prints the letters
-
+ * @filename: filename.
+ * @letters: numbers of letters printed.
  *
  * Return: numbers of letters printed. It fails, returns 0.
  */
--- 100-elf_header.c
+++ 100-elf_header.c
@@ -1,6 +1,6 @@
 /*
  * File: 100-elf_header.c
-
+ * Auth: Brennan D Baraban
  */
 
 #include <elf.h>
